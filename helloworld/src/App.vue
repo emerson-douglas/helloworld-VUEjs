f0b8149447554f644992dab848660df4409a8dcf
Emerson Douglas <63522969+emerson-douglas@users.noreply.github.com>
2021-07-18 15:33:42 -0300

    novo componente
    componente Projetos

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <Pcard :img="dados.avatar_url" :nome="dados.login">
-    <hr><p>projetos:</p><br>
-    <p v-for="repo in repos" :key="repo.id" >
-<span class="lang">
-{{repo.language}}<br>
-</span>
-      {{repo.name}}<br>{{repo.description}}<br>
-      <a :href="repo.html_url">{{ repo.html_url }}</a>
+    <input  name="busca" 
+    id="busca" autocomplete="off" v-model="busca">
+    <button @click="buscar">buscar</button>
+    <Pcard :img="dados.avatar_url" :nome="dados.login" v-show="!imgerro"/>
+
+    <!--componente <erro> !-->
+  <center>
+    <p v-for="repo in repos" :key="repo.id" v-show="!imgerro">
+    <Repo :nome="repo.name" :desc="repo.description" :url="repo.html_url" :lang="repo.language"/>
       </p>
-    </Pcard>
+  </center>
+
     <p>{{ erro }}</p>
-  </div>
+    <img v-show="imgerro" 
+    src="./assets/404.png"/>
+    </div>
 
 
 
@@ -20,13 +24,15 @@
 <script>
 import Api from'./services/api'
 import Pcard from './components/Pcard.vue'
+import Repo from './components/Projetos.vue'
 
 export default {
 data(){
   return{
     dados:[], //array de dados da busca
-    user: 'emerson-douglas', //nome do usuario que você deseja buscar
+    busca: '', //nome do usuario que você deseja buscar
     erro:'',
+    imgerro: false,
     repos:[],
 
   }
@@ -34,21 +40,34 @@ data(){
 
   name: 'App',
     components: {
-    Pcard
+    Pcard,
+    Repo,
   },
 
   created(){
-    //quando a tela é carregada realiza a busca na API
-    Api.get("users/"+this.user)
+    this.buscar()
+
+},
+  methods:{
+    buscar(){
+      //quando a tela é carregada realiza a busca na API
+    Api.get("users/"+this.busca)
     .then(res =>{
+      this.erro=''
+      this.imgerro=false
     this.dados = res.data
     Api.get(this.dados.repos_url) //pega a lista de repositorios
     .then(r=>this.repos = r.data,
     err=>console.log(err))
     },
-    ()=>this.erro="ocorreu um erro ao carregar os dados,tente novamente mais tarde"
+    ()=>{
+      this.dados=[];this.repos=[]
+      this.erro=`ocorreu um erro ao realizar a busca, rocure por um outro usuario`
+      this.imgerro = true}
   )}
-}
+    }
+
+  }
 </script>
 
 <style>
@@ -67,10 +86,6 @@ data(){
   background:#2c3e50 ;
 }
 a{color:white}
-.lang{
-  background: gray;
-  color: black;
-}
 body {
   background: #2c3e50;
 }
---
+++ helloworld/src/components/Projetos.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="repoCard">
+      <span v-show="lang" class="lang"> {{ lang }} </span><br>
+      <a :href="url"><p class="titulo">{{nome}}</p></a>
+<p/>{{ desc }}<br><br>
+  </div>
+</template>
+
+<script>
+export default {
+props:{
+    lang: String,
+    nome: String,
+    desc: String,
+    url: String,
+}
+}
+</script>
+
+<style>
+.repoCard{
+    background: rgb(99, 85, 85);
+    width: fit-content;
+    padding: 10px;
+    border-radius: 10px;
+    transition: opacity 2s;  
+}.repoCard:hover{
+    opacity: 0.2; 
+}
+.lang{
+    background: darkkhaki;
+    color: black;
+    padding: 2px;
+    border-radius: 10px;
+}
+.titulo{
+     font-weight: bold;
+}
+
+</style>
